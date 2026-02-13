53775311777b9348de46735ea01ee087dc941000
spaikid666 <iphonedealvarosalguero@gmail.com>
2026-02-13 18:44:06 +0100

    fixed some changes and now CPP02 ex02 is fully complete

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -1,5 +1,61 @@
 #include "Fixed.hpp"
 
+Fixed::Fixed(void) : _fixedPointValue(0)
+{
+    std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called\n";
+    *this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called\n";
+    if (this != &other)
+        this->_fixedPointValue = other.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called\n";
+}
+
+Fixed::Fixed(const int n)
+{
+    std::cout << "Int constructor called\n";
+    this->_fixedPointValue = n << _fractionalBits;
+}
+
+Fixed::Fixed(const float n)
+{
+    std::cout << "Float constructor called\n";
+    this->_fixedPointValue = static_cast<int>(roundf(n * (1 << _fractionalBits)));
+}
+
+int Fixed::getRawBits(void) const
+{
+    return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_fixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return static_cast<float>(this->_fixedPointValue) / static_cast<float>(1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+    return this->_fixedPointValue >> _fractionalBits;
+}
+
 bool Fixed::operator>(const Fixed &other) const
 {
     return this->getRawBits() > other.getRawBits();
@@ -30,6 +86,46 @@ bool Fixed::operator!=(const Fixed &other) const
     return this->getRawBits() != other.getRawBits();
 }
 
+Fixed Fixed::operator+(const Fixed &other) const
+{
+    Fixed res;
+    res.setRawBits(this->getRawBits() + other.getRawBits());
+    return res;
+}
+
+Fixed Fixed::operator-(const Fixed &other) const
+{
+    Fixed res;
+    res.setRawBits(this->getRawBits() - other.getRawBits());
+    return res;
+}
+
+Fixed Fixed::operator*(const Fixed &other) const
+{
+    Fixed res;
+    long long a = static_cast<long long>(this->getRawBits());
+    long long b = static_cast<long long>(other.getRawBits());
+    long long raw = (a * b) >> _fractionalBits;
+    res.setRawBits(static_cast<int>(raw));
+    return res;
+}
+
+Fixed Fixed::operator/(const Fixed &other) const
+{
+    if (other.getRawBits() == 0)
+    {
+        Fixed res;
+        res.setRawBits(0);
+        return res;
+    }
+
+    Fixed res;
+    long long a = static_cast<long long>(this->getRawBits());
+    long long raw = (a << _fractionalBits) / static_cast<long long>(other.getRawBits());
+    res.setRawBits(static_cast<int>(raw));
+    return res;
+}
+
 Fixed &Fixed::operator++(void)
 {
     this->_fixedPointValue++;
@@ -38,7 +134,7 @@ Fixed &Fixed::operator++(void)
 
 Fixed Fixed::operator++(int)
 {
-    Fixed temp( *this );
+    Fixed temp(*this);
     this->_fixedPointValue++;
     return temp;
 }
@@ -51,7 +147,7 @@ Fixed &Fixed::operator--(void)
 
 Fixed Fixed::operator--(int)
 {
-    Fixed temp( *this );
+    Fixed temp(*this);
     this->_fixedPointValue--;
     return temp;
 }
@@ -74,4 +170,10 @@ Fixed &Fixed::max(Fixed &a, Fixed &b)
 const Fixed &Fixed::max(const Fixed &a, const Fixed &b)
 {
     return (a > b) ? a : b;
+}
+
+std::ostream &operator<<(std::ostream &o, Fixed const &i)
+{
+    o << i.toFloat();
+    return o;
 }
